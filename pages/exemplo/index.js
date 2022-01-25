9142f3ceee0d13804d69fb5d1673320f0bf2248e
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-01-25 20:08:41 -0300

    Font Krub

--- pages/exemplo/index.js
+++ pages/exemplo/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <main className='flex flex-1 w-full h-screen items-center justify-center'>
-        <h1 className='text-5xl text-red-400 text-center'>Olá, Mundo!</h1>
+        <h1 className='text-5xl text-red-400 text-center font-extrabold'>Demon Slayer</h1>
       </main>
     </div>
   )
