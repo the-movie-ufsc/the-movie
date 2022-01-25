64108c926ff5472629d49f73e3768ede9d829557
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-01-25 20:01:08 -0300

    first commit

---
+++ pages/exemplo/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <main className='flex flex-1 w-full h-screen items-center justify-center'>
+        <h1 className='text-5xl text-red-400 text-center'>Olá, Mundo!</h1>
+      </main>
+    </div>
+  )
+}
