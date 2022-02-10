8b6df2c4f0917a31796dc875956bcd62d7bbe6f1
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-02-10 00:19:46 -0300

    Tentando inserir aquela imagem de baixo

--- pages/login/index.js
+++ pages/login/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <main className={styles.container}>
       <h1 className={styles.title}>Login</h1>
       <img src="/logo.svg" className={styles.logo} />
+
       <div className={styles.entrada}>
         <input className={styles.input} placeholder="Email" />
         <input className={styles.input} placeholder="Senha" />
@@ -14,16 +15,21 @@ export default function Home() {
           <p>Esqueceu a sua senha?</p>
         </div>
       </div>
+
       <button className={styles.button}>
         <div>
           Login
-          <img src="/icons/arrow-right.svg" />
+          <img src="/login/icons/arrow-right.svg" />
         </div>
       </button>
 
       <div className={styles.register}>
         <p>Não possui uma conta ainda?</p>
         <a>Registre-se</a>
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.cover}></div>
       </div>
     </main>
   );
