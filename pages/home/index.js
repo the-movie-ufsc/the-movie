e4628fefe778f798bb29c51d72b3089a7bf7720d
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2022-02-11 18:24:30 -0300

    modificacoes gerais

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import styles from "./home.module.css";
-import TMDB from "../TMDB"
-import MoovieRow from "../../components/Shared/MoovieRow";
+import TMDB from "../TMDB";
+import MovieRow from "../../components/Shared/MovieRow";
 import Menu from "../../components/Shared/Menu";
 
 export default function Home() {
   const [movieList, setMovieList] = useState([]);
 
-  useEffect (() => {
-    const loadAll = async () => {         // pegando todas as listas
+  useEffect(() => {
+    const loadAll = async () => {
+      // pegando todas as listas
       let list = await TMDB.getHomeList();
       setMovieList(list);
-    }
+    };
 
     loadAll();
-  }, [])
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -24,12 +25,11 @@ export default function Home() {
 
       {/* Listas */}
       <section className={styles.list}>
-        {movieList.map((item, key)=>(
-          <MoovieRow key={key} title={item.title} items={item.items}/>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
       {/* Footer */}
-
     </div>
   );
 }
