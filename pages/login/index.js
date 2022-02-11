c5e2c1b6a23b159a89c4153203266f355f79c2dc
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-02-11 01:43:42 -0300

    Login atualizado

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,31 +5,33 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>Login</h1>
-      <img src="/logo.svg" className={styles.logo} />
+      <div className={styles.login}>
+        <h1 className={styles.title}>Login</h1>
+        <img src="/logo.svg" className={styles.logo} />
 
-      <div className={styles.entrada}>
-        <input className={styles.input} placeholder="Email" />
-        <input className={styles.input} placeholder="Senha" />
-        <div>
-          <p>Esqueceu a sua senha?</p>
+        <div className={styles.entrada}>
+          <input className={styles.input} placeholder="Email" />
+          <input className={styles.input} placeholder="Senha" />
+          <div>
+            <p>Esqueceu a sua senha?</p>
+          </div>
         </div>
-      </div>
 
-      <button className={styles.button}>
-        <div>
-          Login
-          <img src="/login/icons/arrow-right.svg" />
+        <button className={styles.button}>
+          <div>
+            Login
+            <img src="/login/icons/arrow-right.svg" />
+          </div>
+        </button>
+
+        <div className={styles.register}>
+          <p>Não possui uma conta ainda?</p>
+          <a>Registre-se</a>
         </div>
-      </button>
-
-      <div className={styles.register}>
-        <p>Não possui uma conta ainda?</p>
-        <a>Registre-se</a>
       </div>
 
       <div className={styles.footer}>
-        <div className={styles.cover}></div>
+        <img src="/login/efeito.svg" className={styles.image} />
       </div>
     </main>
   );
