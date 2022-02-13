fa46eadb1a8f4a37db45f68a469091614bcab0ed
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-02-13 00:15:37 -0300

    pag config

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -5,8 +5,31 @@ import styles from "./settings.module.css";
 
 export default function Settings() {
   return (
-    <div className={styles.container}>
+    <>
+      <Head>
+        <title>Settings | The Movie</title>
+        <meta property="og:title" content="Settings The Movie" key="title" />
+      </Head>
+      <main className={styles.frame}>
+        <section className={styles.container}>
+          <h1>Configurações</h1>
+          <h3>Meu Perfil</h3>
+
+          <form className={styles.form}>
+            <div className={styles.inbox}>
+              <label htmlFor="usernameBox">Username</label>
+              <input require className={styles.input} id="usernameBox" />
+            </div>
+            <div className={styles.inbox}>
+              <label htmlFor="emailBox">Email</label>
+              <input type="email" require className={styles.input} id="emailBox" />
+            </div>
+            <p className={styles.changePass}>Alterar senha</p>
+            <button className={styles.save}>Salvar</button>
+          </form>
+        </section>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
