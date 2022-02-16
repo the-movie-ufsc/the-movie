0e615d0012d1cbef7923f121965a2ddf274ffe0b
roger <roger.bambinetti@ozmap.com.br>
2022-02-15 22:30:58 -0300

    adding movies to list

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,15 +1,39 @@
+import React, { useEffect, useState } from "react";
+import MovieRow from "../../components/Shared/MovieRow";
 import styles from "./list.module.css";
 import Menu from "../../components/Shared/Menu";
-import Head from "next/head";
+import TMDB from "../../components/TMDB";
 
 export default function List() {
 
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // pegando todas as listas
+      const list = await TMDB.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  }, []);
+
   return (
+
     <div className={styles.container}>
       <Menu className={styles.menu} />
-      <div className={styles.list}>
-        <h1 className={styles.title}>Minha lista</h1>
-      </div>
+
+      <h1 className={styles.title}>Minha lista</h1>
+
+      {/* Listas */}
+      <section className={styles.list}>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title='' items={item.items} />
+        ))}
+      </section>
+
     </div>
+
+
   );
 }
